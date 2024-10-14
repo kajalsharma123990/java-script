d319e73ffd108b7c4e30a87e5e3f2100c0a76235
kajalsharma123990 <kajalsrn2003@gmail.com>
2024-10-14 11:17:16 +0000

    changes

---
+++ 01_basics/04_datatypeconverson.js
@@ -0,0 +1,27 @@
+let name = "kajal"
+let booleanValue= Boolean(name)
+
+// console.log(booleanValue)
+
+let score = 33
+console.log(typeof score)
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+
+// 33 number value easily converted   into number
+// 33abc   number converted NaN  means not a number
+// true  value converted  1  
+// false value conveted 2
+
+// "kajal"  converted  true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber)
+
+let number = "33abc"
+let numberValue = Number(number)
+console.log( numberValue)
