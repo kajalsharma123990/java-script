f91312149afac43ace465a148d020cf60c614e4f
kajalsharma123990 <kajalsrn2003@gmail.com>
2024-10-15 05:30:14 +0000

    datatype-summary

---
+++ 01_basics/datatype.js
@@ -0,0 +1,31 @@
+
+// Primitive datatype
+
+let score = 100 //number
+let name = "abc"//string
+let Isfollow = true //boolean
+let outsideTemp = null  //null
+let userEmail;  //undefined
+let id = Symbol('123') //symbol
+
+let bigNumber = 1234586424554562n //bigInt
+
+
+//Non primitive  datatype
+
+// Array
+
+const student= ["name", "RollNo"]
+
+// object
+
+const myObj = {
+    name: "kajal",
+    age:21,
+}
+
+//function
+
+const myFunction = function(){
+    console.log("kajal")
+}
