bbc88a1e629671f5d2e5a26669948a1bfb2a1b7f
kajalsharma123990 <kajalsrn2003@gmail.com>
2024-10-15 10:18:47 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+
+
+let newCreateDate = new Date("10-15-2024")
+// console.log(newCreateDate.toLocaleString())
+
+
+//  timestamp
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+let date = new Date()
+// console.log(date.getMonth())
+
+newCreateDate.toLocaleString("default", {
+    weekday: "long",
+    
+})
+
